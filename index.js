4d0fdfd85147a088fc2e6487c7de89911f73173c
Joseph Safari <sajo2460hotmail.com>
2019-11-25 22:26:53 +0000

    Add keys to calculator

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', init);
 const opts = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.']; //all keys
 const spec = ['*', '/', '+', '-']; //special function keys
+function addOutput(e) {
+  console.log(e);
+}
+ 
 function init() {
   document.title = "JavaScript Project";
   console.log('ready');
@@ -21,4 +25,21 @@ function init() {
   main.classList.add('main');
   main.style.width = '100%';
   container.appendChild(main);
+  opts.forEach(function (val) {
+    console.log(val);
+    btnMaker(val, addOutput);
+  })
+ 
+  function btnMaker(txt, myFunction) {
+    let btn = document.createElement('button');
+    btn.setAttribute('type', 'buttom');
+    btn.style.width = '23%';
+    btn.style.lineHeight = '50px';
+    btn.style.margin = '1%';
+    btn.style.fontSize = '2em';
+    btn.val = txt;
+    btn.textContent = txt;
+    btn.addEventListener('click', myFunction);
+    main.appendChild(btn);
+  }
 }
