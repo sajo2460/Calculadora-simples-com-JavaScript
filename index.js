3aaa50c3fa33ee30c12e9c2dfccd058247271c58
Joseph Safari <sajo2460hotmail.com>
2019-11-25 22:23:15 +0000

    Setup calculator values

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-window.addEventListener('DOMContentLoaded',init);
-function init(){
+window.addEventListener('DOMContentLoaded', init);
+const opts = ['*', '/', '+', '-', '9', '8', '7', '6', '5', '4', '3', '2', '1', '0', '.']; //all keys
+const spec = ['*', '/', '+', '-']; //special function keys
+function init() {
+  document.title = "JavaScript Project";
   console.log('ready');
+  const container = document.createElement('div');
+  container.classList.add('container');
+  container.style.maxWidth = '600px';
+  container.style.margin = 'auto';
+  document.body.appendChild(container);
+  const output = document.createElement('input');
+  output.setAttribute('type', 'text');
+  output.classList.add('output');
+  output.style.width = '100%';
+  output.style.lineHeight = '50px';
+  output.style.fontSize = '3em';
+  output.style.textAlign = 'right';
+  container.appendChild(output);
+  const main = document.createElement('div');
+  main.classList.add('main');
+  main.style.width = '100%';
+  container.appendChild(main);
 }
